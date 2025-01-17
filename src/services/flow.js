a9997607b58b2dad7f98afa321c6cc99fa5a7ef1
Raphael Serafim <raphaelvserafim@gmail.com>
2025-01-17 13:53:30 -0500

    ♻️ refactor: Flow Services

--- src/services/flow.js
+++ src/services/flow.js
@@ -3,7 +3,6 @@ import httpService from "@theflow/services/api";
 
 export class FlowService {
 
-
   static async updateFlow(code, data) {
     const response = await httpService.put(environment.API.FLOW.INFO + "/" + code, data);
     return response.data;
@@ -15,73 +14,44 @@ export class FlowService {
   }
 
   static async saveNodes(code, data) {
-    try {
-      const response = await httpService.post(environment.API.FLOW.INFO + "/" + code + "/nodes", data);
-      return response.data;
-    } catch (error) {
-      return { status: 500, message: error.message }
-    }
+    const response = await httpService.post(environment.API.FLOW.INFO + "/" + code + "/nodes", data);
+    return response.data;
   }
 
   static async updateNodes(code, code_nodes, data) {
-    try {
-      const response = await httpService.put(environment.API.FLOW.INFO + "/" + code + "/nodes/" + code_nodes, data);
-      return response.data;
-    } catch (error) {
-      return { status: 500, message: error.message }
-    }
+    const response = await httpService.put(environment.API.FLOW.INFO + "/" + code + "/nodes/" + code_nodes, data);
+    return response.data;
   }
 
   static async updateContentNodes(code, code_nodes, data) {
-    try {
-      const response = await httpService.put(environment.API.FLOW.INFO + "/" + code + "/nodes/" + code_nodes + "/content", data);
-      return response.data;
-    } catch (error) {
-      return { status: 500, message: error.message }
-    }
+    const response = await httpService.put(environment.API.FLOW.INFO + "/" + code + "/nodes/" + code_nodes + "/content", data);
+    return response.data;
   }
 
   static async uploadFileNodes(code, code_nodes, data) {
-    try {
-      const response = await httpService.put(environment.API.FLOW.INFO + "/" + code + "/nodes/" + code_nodes + "/content", data, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      return response.data;
-    } catch (error) {
-      console.error('Error updating content nodes:', error);
-      return { status: 500, message: error.message };
-    }
+    const response = await httpService.put(environment.API.FLOW.INFO + "/" + code + "/nodes/" + code_nodes + "/content", data, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    return response.data;
   }
 
   static async deleteNodes(code, code_nodes) {
-    try {
-      const response = await httpService.delete(environment.API.FLOW.INFO + "/" + code + "/nodes/" + code_nodes);
-      return response.data;
-    } catch (error) {
-      return { status: 500, message: error.message }
-    }
+    const response = await httpService.delete(environment.API.FLOW.INFO + "/" + code + "/nodes/" + code_nodes);
+    return response.data;
   }
 
 
   static async connectEdges(code, data) {
-    try {
-      const response = await httpService.post(environment.API.FLOW.INFO + "/" + code + "/edges", data);
-      return response.data;
-    } catch (error) {
-      return { status: 500, message: error.message }
-    }
+    const response = await httpService.post(environment.API.FLOW.INFO + "/" + code + "/edges", data);
+    return response.data;
   }
 
 
   static async deleteEdges(code, source, target) {
-    try {
-      const response = await httpService.delete(environment.API.FLOW.INFO + "/" + code + "/edges/" + source + "/" + target);
-      return response.data;
-    } catch (error) {
-      return { status: 500, message: error.message }
-    }
+    const response = await httpService.delete(environment.API.FLOW.INFO + "/" + code + "/edges/" + source + "/" + target);
+    return response.data;
   }
 
 }
